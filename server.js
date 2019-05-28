e0b4fc37d85c31ebae6515e56aa1691797f029e8
skyepodium <skyepodium@gmail.com>
2019-05-29 00:14:26 +0900

    add back_end server logic

--- server.js
+++ server.js
@@ -6,3 +6,21 @@ app.get(/.*/, (req, res) => {
     res.sendfile(__dirname + "/dist/index.html")
 })
 app.listen(80);
+
+const http = require('http')
+const socket = require('socket.io')
+
+let server = http.createServer((req, res) => {
+}).listen(3000);
+
+const io = socket(server)
+
+io.on('connection', (socket) => {
+    socket.on('chat', (msg) => {
+        let data = JSON.parse(msg)
+        console.log(data)
+        console.log(data.name)
+        console.log(data.message)
+        io.emit('chat', data)
+    })
+})
