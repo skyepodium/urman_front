298ea88e9fdae3d7ccc652652a3db93d840345d6
skyepodium <skyepodium@gmail.com>
2019-06-02 17:37:41 +0900

    remove comment

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
-// app.use(express.static(__dirname + "/dist/"));
-// app.get(/.*/, (req, res) => {
-//     res.sendfile(__dirname + "/dist/index.html")
-// })
-// app.listen(80);
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, (req, res) => {
+    res.sendfile(__dirname + "/dist/index.html")
+})
+app.listen(80);
 
 const http = require('http')
 const socket = require('socket.io')
